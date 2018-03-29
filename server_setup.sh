1f8749ee0eb9c2d6fdcfbd0a2cbefce6bee36d1f
Ad!thya R <adithya.r02@outlook.com>
2018-03-29 21:02:14 +0530

    Start the script in the home dir

--- server_setup.sh
+++ server_setup.sh
@@ -1,3 +1,5 @@
+cd ~
+
 # Install the dependencies
 sudo apt-get update
 sudo apt-get install -y bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline6-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev unzip openjdk-8-jdk python
