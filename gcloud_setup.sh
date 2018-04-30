bab0201f18c3653538b598d040c2d884cacd4287
Ad!thya R <adithya.r02@outlook.com>
2018-04-30 09:12:23 +0530

    Fix tinfo6 installation and be more concise

--- gcloud_setup.sh
+++ gcloud_setup.sh
@@ -10,26 +10,43 @@
 cd ~
 
 # Install the dependencies
+echo ==========Updating system==============
+echo   
 sudo apt-get update
+echo   
+echo =========Installing dependencies========
+echo    
 sudo apt-get install --yes --force-yes bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline6-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev unzip openjdk-8-jdk python
 sudo apt-get upgrade
 
 # Install libtinfo6 (required for GCC 7.x and above)
-wget http://ftp.us.debian.org/debian/pool/main/n/ncurses/lib32tinfo6_6.1+20180210-1_amd64.deb
-sudo dpkg -i lib32tinfo6_6.1+20180210-1_amd64.deb
-wget http://ftp.us.debian.org/debian/pool/main/n/ncurses/libtinfo6_6.1+20180210-1_amd64.deb
-sudo dpkg -i libtinfo6_6.1+20180210-1_amd64.deb
+echo    
+echo ==========Installing libtinfo6============
+echo      
+wget http://ftp.debian.org/debian/pool/main///n/ncurses/lib32tinfo6_6.1+20180210-2_amd64.deb
+sudo dpkg -i lib32tinfo6_6.1+20180210-2_amd64.deb
+wget http://ftp.debian.org/debian/pool/main///n/ncurses/libtinfo6_6.1+20180210-2_amd64.deb
+sudo dpkg -i libtinfo6_6.1+20180210-2_amd64.deb
 
 # Install Android SDK
+echo   
+echo ===========Installing Android SDK===========
+echo   
 wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
 unzip platform-tools-latest-linux.zip
 
 # Install repo
+echo   
+echo ===========Installing repo tool==============
+echo    
 mkdir bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 
 # Add env variables to bashrc
+echo    
+echo ========Updating bashrc and .profile===========
+echo     
 cat <<EOT >> ~/.bashrc
 
 export USE_CCACHE=1
@@ -57,6 +74,9 @@ source ~/.profile
 source ~/.bashrc
 
 # Configure git
+echo    
+echo ===========Configuring git=============
+echo    
 git config --global user.email "adithya.r02@outlook.com"
 git config --global user.name "Adithya R"
 git config --global alias.cp 'cherry-pick -s'
