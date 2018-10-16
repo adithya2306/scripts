e62fa001610e48b09195494150eb18e4fae4d449
Ad!thya R <adithya.r02@outlook.com>
2018-10-16 09:43:59 +0530

    do_setup: Update for ubuntu 18.04 droplets

--- digitalocean_setup.sh
+++ digitalocean_setup.sh
@@ -11,14 +11,8 @@ cd ~
 
 # Install the dependencies
 sudo apt-get update
-sudo apt-get install -y bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline6-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev unzip openjdk-8-jdk python
-sudo apt-get upgrade
-
-# Install libtinfo6 (required for GCC 7.x and above)
-wget http://ftp.us.debian.org/debian/pool/main/n/ncurses/lib32tinfo6_6.1+20180210-1_amd64.deb
-dpkg -i lib32tinfo6_6.1+20180210-1_amd64.deb
-wget http://ftp.us.debian.org/debian/pool/main/n/ncurses/libtinfo6_6.1+20180210-1_amd64.deb
-dpkg -i libtinfo6_6.1+20180210-1_amd64.deb
+sudo apt-get install -y bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev unzip openjdk-8-jdk python ccache
+sudo apt-get upgrade -y
 
 # Install Android SDK
 wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
@@ -30,7 +24,7 @@ curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 
 # Add env variables to bashrc
-cat <<EOT >> ~/.bashrc
+cat <<'EOF' >> ~/.bashrc
 
 export USE_CCACHE=1
 export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx6144m"
@@ -39,7 +33,7 @@ export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF8 -Xmx6144m"
 EOT
 
 # Add ~/bin and sdk to path
-cat <<EOT >> ~/.profile
+cat <<'EOF' >> ~/.profile
 
 # set PATH so it includes user's private bin if it exists
 if [ -d "$HOME/bin" ] ; then
@@ -50,19 +44,19 @@ fi
 if [ -d "$HOME/platform-tools" ] ; then
     PATH="$HOME/platform-tools:$PATH"
 fi
-EOT
+EOF
 
 # Set env from .bashrc and .profile
 source ~/.profile
 source ~/.bashrc
 
 # Configure git
-git config --global user.email "adithya.r02@outlook.com"
+git config --global user.email "gh0strider.2k18.reborn@gmail.com"
 git config --global user.name "Adithya R"
-git config --global alias.cp 'cherry-pick -s'
-git config --global alias.c 'commit -s'
+git config --global alias.cp 'cherry-pick'
+git config --global alias.c 'commit'
 
 # Done!
 echo
-echo Everything done.
+echo "All done. Now sync sauces & start baking!"
 echo
