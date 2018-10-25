eca3323ad3705e2c68b87cf8c964afe3a792eb35
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2018-10-25 21:46:41 +0530

    Add a script to clone device sources of lettuce

---
+++ clone_lettuce_repos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Script to clone device sources for lettuce
+
+read -p "Enter branch name of device tree (default lineage-16.0): " DEVICE_BRANCH
+read -p "Enter branch name of remaining trees (default lineage-16.0): " BRANCH
+
+if [-z "$BRANCH"]; then echo "lol"; fi
+if [-z "$DEVICE_BRANCH"]; then BRANCH="lineage-16.0"; fi
+
+echo -e "\n============== CLONING DEVICE TREE ==============\n"
+git clone -b $DEVICE_BRANCH https://github.com/lettuce-pie/android_device_yu_lettuce device/yu/lettuce
+
+echo -e "\n============== CLONING VENDOR TREE ==============\n"
+git clone -b $BRANCH https://github.com/lettuce-pie/proprietary_vendor_yu vendor/yu
+
+echo -e "\n============== CLONING KERNEL ==============\n"
+git clone -b $BRANCH https://github.com/lettuce-pie/android_kernel_cyanogen_msm8916 kernel/cyanogen/msm8916
+
+echo -e "\n============== DONE ==============\n"
