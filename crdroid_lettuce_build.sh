8a28016ff10830948fc717ac610a9e7cd8e606a3
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2018-11-26 16:41:09 +0530

    cr_lettuce_build: Support auto upload of zip to gdrive
    * with error detection

--- crdroid_lettuce_build.sh
+++ crdroid_lettuce_build.sh
@@ -18,5 +18,18 @@ elif [[ $CR_BUILD_TYPE = "superclean" ]]; then
 fi
 
 . build/envsetup.sh
+
+BUILDDATE=$(date +%Y%m%d)
 lunch lineage_lettuce-userdebug
+
 mka bacon
+
+if [ $? -eq 0 ]; then
+    echo -e "\nROM compiled succesfully :-) Now uploading ROM zip to gdrive ...\n"
+    gdrive upload $CR_DIR/out/target/product/lettuce/crDroidAndroid-9.0-$BUILDDATE-*
+    if [ $? -eq 0 ]; then
+        echo -e "\nERROR occured while uploading ROM zip to gdrive :< EXITING ..."
+    fi
+else
+    echo -e "\nERROR OCCURED DURING COMPILATION :'( EXITING ..."
+fi
