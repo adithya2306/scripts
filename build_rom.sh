0c9d280e9770565925114d5916572d954557c42b
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2018-12-10 16:29:05 +0530

    build_rom: New script to build any android ROM for any device
    * Support uploading and jenkins build type parameter too! (check the script comments for more details)

---
+++ build_rom.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#
+# Script to build any android ROM for any device and upload it to transfer.sh.
+# Not recommended for build butts.
+#
+# Usage (in root of source):
+#   ./build_rom.sh <lunch_command> <build_type> MAKE_TARGET=<make_target> OUT_DIR=<out_dir> CCACHE_DIR=<ccache_dir> 
+#
+# <build_type> can be any of the following [set to "default" by default]:
+#   "default": Performs a normal build i.e. does not delete the output dir before building.
+#   "clean": Performs a clean build by deleting outdir before compiling.
+#   "superclean": Performs a 100% clean build by deleting outdir and ccache dir before compiling.
+#
+# ProTip™: To set build type in a jenkins project, make a choice parameter with these values and
+#          pass the choice name variable as argument.
+#
+# MAKE_TARGET is an optional flag specifying the name of the target to compile, and defaults to "bacon" (i.e. the target 
+# name of the final flashable ROM zip). In case of AOSP/CAF-based ROMs where usually the command is "otapackage" and 
+# not "bacon", set this flag accordingly. It can also be set to "bootimage" to compile the boot.img only.
+#
+# OUT_DIR and CCACHE_DIR too are optional arguments which can be used to specify the ccache dir and the build 
+# output dir respectively. Defaults to "./out" and "~/.ccache" respectively. They should not contain spaces in between.
+#
+
+if [[ -z "$1" ]]; then echo "ERROR: Lunch command not specified! Aborting ..."; exit 1; fi
+
+LUNCH=$1
+DEVICE=$(sed -e "s/^.*_//" -e "s/-.*//" <<< $LUNCH)
+
+if [ -n "$2" ]; then
+    if [[ $2 = "default" ]] || [[ $2 = "clean" ]] || [[ $2 = "superclean" ]]; then
+        BUILD_TYPE=$2
+    else
+        echo "ERROR: Invalid build type: $2. Aborting ..."
+        exit 1
+    fi
+else
+    BUILD_TYPE="default"
+fi
+
+if [[ -z "$MAKE_TARGET" ]]; then MAKE_TARGET="bacon"; fi
+
+if [ -n "$CCACHE_DIR" ]; then export CCACHE_DIR=$CCACHE_DIR; fi
+
+if [ -n "$OUT_DIR" ]; then export OUT_DIR_COMMON_BASE=$OUT_DIR; else OUT_DIR=out; fi
+
+# Set java compilation mem usage limit to 4GB if system has RAM lesser than 18GB
+if [ $(($(grep MemTotal /proc/meminfo | awk '{print $2}')/(1024 * 1024))) -lt 18 ]; then 
+    export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
+    export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
+    export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF8 -XX:+TieredCompilation -Xmx4g"
+fi
+
+# Enable CCACHE
+export USE_CCACHE=1
+export CCACHE_NOCOMPRESS=true
+
+echo -e "\nDevice: $DEVICE \nROM Source directory: $(pwd) \nCCACHE directory: $CCACHE_DIR\n"
+
+if [[ $BUILD_TYPE = "clean" ]]; then
+    echo -e "Clean build selected. Clearing outdir\n"
+    rm -rf out
+elif [[ $BUILD_TYPE = "superclean" ]]; then
+    echo -e "Superclean build selected. Clearing outdir & ccache dir\n"
+    ccache -C
+    rm -rf out
+fi
+
+. build/envsetup.sh
+lunch $LUNCH
+
+# Equivalent of "mka" command, modified to use 2 x (no. of cores) threads for compilation
+schedtool -B -n 1 -e ionice -n 1 make -j$(($(nproc --all) * 2)) $MAKE_TARGET
+
+if [ $? -eq 0 ] && [[ $MAKE_TARGET != "bootimage" ]]; then
+    echo -e "\nROM compiled succesfully :-) Now uploading ROM zip to transfer.sh ...\n"
+    
+    if [ $(ls $OUT_DIR/target/product/$DEVICE/*.zip | wc -l) -gt 1 ]; then
+        zippath=$(sed "s/\.md5sum//" <<< $(ls $OUT_DIR/target/product/$DEVICE/*.md5sum))
+    else
+        zippath=$(ls $OUT_DIR/target/product/$DEVICE/*.zip)
+    fi
+    
+    echo -e "ROM uploaded succesfully to $(curl -sT $zippath https://transfer.sh/$(basename $zippath)) \n"
+    exit 0
+else
+    echo -e "\nERROR OCCURED DURING COMPILATION :'( EXITING ..."
+    exit 1
+fi
