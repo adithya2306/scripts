2ee358ab354f56fe1470aa4bd7402615edf08c53
FacuM <facumo.fm@gmail.com>
2019-01-30 05:07:22 -0300

    clone_lettuce_repos: simplify URLs, allow changing the organization URL.

--- clone_lettuce_repos.sh
+++ clone_lettuce_repos.sh
@@ -4,16 +4,17 @@
 read -p "Enter branch name of device tree (default lineage-16.0): " DEVICE_BRANCH
 read -p "Enter branch name of remaining trees (default lineage-16.0): " BRANCH
 
+ORGANIZATION='https://github.com/lettuce-pie'
 if [ -z "$BRANCH"]; then BRANCH="lineage-16.0"; fi
 if [ -z "$DEVICE_BRANCH"]; then DEVICE_BRANCH="lineage-16.0"; fi
 
 echo -e "\n============== CLONING DEVICE TREE ==============\n"
-git clone -b $DEVICE_BRANCH https://github.com/lettuce-pie/android_device_yu_lettuce device/yu/lettuce
+git clone -b $DEVICE_BRANCH "$ORGANIZATION"'/android_device_yu_lettuce' device/yu/lettuce
 
 echo -e "\n============== CLONING VENDOR TREE ==============\n"
-git clone -b $BRANCH https://github.com/lettuce-pie/proprietary_vendor_yu vendor/yu
+git clone -b $BRANCH "$ORGANIZATION"'/proprietary_vendor_yu' vendor/yu
 
 echo -e "\n============== CLONING KERNEL ==============\n"
-git clone -b $BRANCH https://github.com/lettuce-pie/android_kernel_cyanogen_msm8916 kernel/cyanogen/msm8916
+git clone -b $BRANCH "$ORGANIZATION"'/android_kernel_cyanogen_msm8916' kernel/cyanogen/msm8916
 
 echo -e "\n============== DONE ==============\n"
