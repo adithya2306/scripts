c36721c2c95cad008022a430a5bce735e5c8d497
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2019-02-12 18:46:56 +0530

    ubuntu_setup: Install libtinfo5 for Ubuntu 18.10
    * It's an old legacy library which is present until Ubuntu 18.04 but removed from 
      18.10 onwards. Clang 6 provided by AOSP uses this library and fails to run if it's 
      absent. Thx to @mg712702 for reporting the issue.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -13,7 +13,7 @@ cd ~
 # Installing packages
 echo -e "\n================== INSTALLING & CONFIGURING PACKAGES ==================\n"
 sudo apt-get update
-sudo apt-get install -y bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev unzip openjdk-8-jdk python ccache
+sudo apt-get install -y bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev unzip openjdk-8-jdk python ccache libtinfo5
 sudo apt-get upgrade -y
 
 # CCache hax (unlimited ccache)
