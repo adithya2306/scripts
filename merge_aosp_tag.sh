58cf85b13d43315f1832855a655cc92332e841b7
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2019-03-09 18:13:59 +0530

    merge_aosp_tag: Add an additional check for empty merge
    * Hash checking always doesnt work cuz sometimes AOSP creates different merge commits
      but with different commit ID thereby changing the HEAD hash without updating source.
    
    * Example of such an empty merge: https://github.com/FredProject/platform_external_tremolo/commit/e9faac4579bf935ee3a88308de797420474c0c38 ._.
    
    [v2 edits: revert the merge if its empty
               add a todo to support whitelisted repo paths
               "additional remote" makes more sense than "caf remote"]

--- merge_aosp_tag.sh
+++ merge_aosp_tag.sh
@@ -3,19 +3,22 @@
 # Script to merge specified AOSP tag in repos tracked by Google's repo tool (git-repo)
 #
 # Usage (in root of ROM source):
-#       repo forall -c $(pwd)/merge_aosp_tag.sh <tag_name> <aosp_remote_name> <caf_remote_name>
+#       repo forall -c $(pwd)/merge_aosp_tag.sh <tag_name> <aosp_remote_name> [<additional_remote_name>]
 #
-# NOTE: if CAF remote doesnt exist then leave it as "caf"
 
 TAG=$1
 AOSP_REMOTE=$2
-CAF_REMOTE=$3
+if [ -n "$3" ]; then ADDNL_REMOTE=$3, else ADDNL_REMOTE="k"; fi
 
-if [[ $REPO_REMOTE != $AOSP_REMOTE && $REPO_REMOTE != $CAF_REMOTE ]]; then
+###
+### TODO: Whitelisted repos support
+###
+
+if [[ $REPO_REMOTE != $AOSP_REMOTE && $REPO_REMOTE != $ADDNL_REMOTE ]]; then
 
         # Workaround for build/make as it lies in "platform/build" repo in AOSP
         if [[ $REPO_PATH = "build/make" ]]; then REPO_PATH="build"; fi
-            
+
         # Check if it is a repo which is forked from AOSP
         wget -q --spider https://android.googlesource.com/platform/$REPO_PATH
 
@@ -33,9 +36,9 @@ if [[ $REPO_REMOTE != $AOSP_REMOTE && $REPO_REMOTE != $CAF_REMOTE ]]; then
                 # Merge and inform user on succesful merge, by comparing hash
                 git merge -q --no-ff -m "Merge tag '$TAG' into $branch" FETCH_HEAD
                 if [ $? -eq 0 ]; then
-                        if [[ $(git rev-parse HEAD) != $hash ]]; then
+                        if [[ $(git rev-parse HEAD) != $hash ]] && [[ $(git diff HEAD $REPO_REMOTE/$branch) ]]; then
                                 echo -e "\n\e[34m$REPO_PATH merged succesfully\e[0m\n"
-                        fi
+                        else git reset --hard $REPO_REMOTE/$branch; fi
                 else
                         echo -e "\n\e[31m$REPO_PATH has merge errors\e[0m\n"
                 fi
