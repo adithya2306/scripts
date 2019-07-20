cf2ea22cd5b480ee0f90bfd49499d01abbd29bff
Ad!thya R <adithya.r02@outlook.com>
2019-07-20 17:35:41 +0530

    Update merge-aosp-tag.sh

--- merge-aosp-tag.sh
+++ merge-aosp-tag.sh
@@ -14,31 +14,28 @@ if [ -n "$3" ]; then ADDNL_REMOTE=$3, else ADDNL_REMOTE="k"; fi
 ### TODO: Whitelisted repos support
 ###
 
-if [[ $REPO_REMOTE != $AOSP_REMOTE && $REPO_REMOTE != $ADDNL_REMOTE ]]; then
+if [[ $REPO_REMOTE != "$AOSP_REMOTE" && $REPO_REMOTE != "$ADDNL_REMOTE" ]]; then
 
         # Workaround for build/make as it lies in "platform/build" repo in AOSP
         if [[ $REPO_PATH = "build/make" ]]; then REPO_PATH="build"; fi
 
         # Check if it is a repo which is forked from AOSP
-        wget -q --spider https://android.googlesource.com/platform/$REPO_PATH
-
-        if [ $? -eq 0 ]; then
+        if wget -q --spider https://android.googlesource.com/platform/$REPO_PATH; then
                 # Find branch name from manifest & checkout
-                branch=$(sed 's|refs\/heads\/||' <<< $REPO_RREV)
-                git checkout -q $branch
+                branch=$(sed 's|refs\/heads\/||' <<< "$REPO_RREV")
+                git checkout -q "$branch"
 
                 # Fetch the tag from AOSP
-                git fetch -q https://android.googlesource.com/platform/$REPO_PATH $TAG
+                git fetch -q https://android.googlesource.com/platform/$REPO_PATH "$TAG"
 
                 # Store the current hash value of HEAD
                 hash=$(git rev-parse HEAD)
 
                 # Merge and inform user on succesful merge, by comparing hash
-                git merge -q --no-ff -m "Merge tag '$TAG' into $branch" FETCH_HEAD
-                if [ $? -eq 0 ]; then
-                        if [[ $(git rev-parse HEAD) != $hash ]] && [[ $(git diff HEAD $REPO_REMOTE/$branch) ]]; then
+                if git merge -q --no-ff -m "Merge tag '$TAG' into $branch" FETCH_HEAD; then
+                        if [[ $(git rev-parse HEAD) != "$hash" ]] && [[ $(git diff HEAD "$REPO_REMOTE"/"$branch") ]]; then
                                 echo -e "\n\e[34m$REPO_PATH merged succesfully\e[0m\n"
-                        else git reset --hard $REPO_REMOTE/$branch; fi
+                        else git reset --hard "$REPO_REMOTE"/"$branch"; fi
                 else
                         echo -e "\n\e[31m$REPO_PATH has merge errors\e[0m\n"
                 fi
