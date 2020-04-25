3357f15c5de1ee1e7ad9227e0fcb6813e72f8c75
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2020-04-25 21:41:25 +0530

    ubuntu-setup: Don't install git-lfs in ubuntu 20.04
    seems like they didn't add support for 20.04 yet:
    
    Err:7 https://packagecloud.io/github/git-lfs/ubuntu focal Release
      404  Not Found [IP: 54.183.113.236 443]
    Reading package lists... Done
    E: The repository 'https://packagecloud.io/github/git-lfs/ubuntu focal Release' does not have a Release file.
    N: Updating from such a repository can't be done securely, and is therefore disabled by default.
    N: See apt-secure(8) manpage for repository creation and user configuration details.

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -18,17 +18,17 @@ cd ~ || return
 echo -e "\n================== INSTALLING & CONFIGURING PACKAGES ==================\n"
 sudo apt -qq update
 sudo apt full-upgrade -y -qq
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt install -y -qq bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf \
                         imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool \
                         libncurses5-dev libsdl1.2-dev libxml2 libxml2-utils lzop pngcrush \
                         schedtool squashfs-tools xsltproc zip zlib1g-dev unzip openjdk-8-jdk python ccache \
-                        libtinfo5 libncurses5 android-tools-adb git-lfs tmux libssl-dev
+                        libtinfo5 libncurses5 android-tools-adb tmux libssl-dev neofetch
 
 if [[ $(lsb_release -rs) == "20"* ]]; then
 sudo apt install -y -qq libwxgtk3.0-gtk3-dev
 else
-sudo apt install -y -qq libwxgtk3.0-dev
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt install -y -qq libwxgtk3.0-dev git-lfs
 fi
 
 sudo apt autoremove -y -qq
