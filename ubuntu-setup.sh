f44cdc8683febb41888f81ec81d83ed8a7dc1e56
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2020-08-24 21:39:37 +0530

    ubuntu-setup: Install patchelf

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -22,7 +22,7 @@ sudo apt install -y -qq bc bison build-essential curl flex g++-multilib gcc-mult
                         imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool \
                         libncurses5-dev libsdl1.2-dev libxml2 libxml2-utils lzop pngcrush \
                         schedtool squashfs-tools xsltproc zip zlib1g-dev unzip openjdk-8-jdk python ccache \
-                        libtinfo5 libncurses5 android-tools-adb tmux libssl-dev neofetch
+                        libtinfo5 libncurses5 android-tools-adb tmux libssl-dev neofetch patchelf
 
 if [[ $(lsb_release -rs) == "20"* ]]; then
 sudo apt install -y -qq libwxgtk3.0-gtk3-dev
