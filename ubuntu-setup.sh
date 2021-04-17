dabe68353fcaec1ea48eea67ea8d02d080925b54
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-04-17 16:51:22 +0530

    ubuntu-setup: Cleanup and rework

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,82 +1,68 @@
 #!/bin/bash
 #
-# Script to set up an Ubuntu 16.04+ server or PC
-# (with minimum 8GB RAM, 4 cores CPU) for android ROM compiling
+# Script to set up an Ubuntu 20.04+ server or PC
+# (with minimum 16GB RAM, 4 cores CPU) for android ROM compiling
 #
-# IMPORTANT NOTICE: This script sets my personal git config, update 
+# Sudo access is mandatory to run this script
+#
+# IMPORTANT NOTICE: This script sets my personal git config, update
 # it with your details before you run this script!
 #
 # Usage:
-#	./ubuntu_setup.sh
+#	./ubuntu-setup.sh
 #
 
 # Go to home dir
 orig_dir=$(pwd)
-cd ~ || return
+cd $HOME
 
-# Installing packages
-echo -e "\n================== INSTALLING & CONFIGURING PACKAGES ==================\n"
+echo -e "Installing and updating packages...\n"
 sudo apt -qq update
 sudo apt full-upgrade -y -qq
-sudo apt install -y -qq bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf \
-                        imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool \
-                        libncurses5-dev libsdl1.2-dev libxml2 libxml2-utils lzop pngcrush \
-                        schedtool squashfs-tools xsltproc zip zlib1g-dev unzip openjdk-8-jdk python ccache \
-                        libtinfo5 libncurses5 android-tools-adb tmux libssl-dev neofetch patchelf apktool \
-                        python-dev python3-dev dos2unix jq
-
-if [[ $(lsb_release -rs) == "20"* ]]; then
-sudo apt install -y -qq libwxgtk3.0-gtk3-dev
-else
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt install -y -qq libwxgtk3.0-dev git-lfs
-fi
-
+sudo apt install -y -qq git-core gnupg flex bc bison build-essential zip curl zlib1g-dev gcc-multilib \
+                        g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev jq \
+                        lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig imagemagick \
+                        python2 python3 python3-pip python3-dev python-is-python3 schedtool ccache libtinfo5 \
+                        libncurses5 lzop tmux libssl-dev neofetch patchelf apktool dos2unix git-lfs default-jdk
 sudo apt autoremove -y -qq
 
-# Install git-repo
-mkdir bin
-curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-chmod a+x ~/bin/repo
+wget -q https://storage.googleapis.com/git-repo-downloads/repo
+chmod a+x repo
+sudo install repo /usr/local/bin/repo
+rm repo
+echo -e "\nDone."
 
-# CCache hax (unlimited ccache)
-ccache -M 500G
-
-# Install Android SDK
-echo -e "\n================== INSTALLING ANDROID SDK ==================\n"
-wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-unzip platform-tools-latest-linux.zip
+echo -e "\nInstalling Android SDK platform tools..."
+wget -q https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+unzip platform-tools-latest-linux.zip &> /dev/null
 rm platform-tools-latest-linux.zip
+echo -e "Done."
 
-# Install google drive command line tool
-echo -e "\n================== INSTALLING GDRIVE CLI ==================\n"
-wget https://raw.githubusercontent.com/usmanmughalji/gdriveupload/master/gdrive
+echo -e "\nInstalling Google Drive CLI..."
+wget -q https://raw.githubusercontent.com/usmanmughalji/gdriveupload/master/gdrive
 chmod a+x gdrive
 sudo install gdrive /usr/local/bin/gdrive
 rm gdrive
+echo -e "Done."
 
-# Set up environment
-echo -e "\n================== SETTING UP ENV ==================\n"
+echo -e "\nSetting up shell environment..."
 if [[ $SHELL = *zsh* ]]; then
-sh_rc="$HOME/.zshrc"
+sh_rc=".zshrc"
 else
-sh_rc="$HOME/.bashrc"
+sh_rc=".bashrc"
 fi
 
 cat <<'EOF' >> $sh_rc
- 
+
 # Upload a file to transfer.sh
-transfer() { if [ $# -eq 0 ]; then echo -e "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi 
-tmpfile=$( mktemp -t transferXXX ); if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; else curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; fi; cat $tmpfile; rm -f $tmpfile; } 
- 
+transfer() { if [ $# -eq 0 ]; then echo -e "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi
+tmpfile=$( mktemp -t transferXXX ); if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; else curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; fi; cat $tmpfile; rm -f $tmpfile; }
+
 # Super-fast repo sync
-repofastsync() { time schedtool -B -n 0 -e ionice -n 0 `which repo` sync -c -q --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) "$@"; }
+repofastsync() { time schedtool -B -e ionice -n 0 `which repo` sync -c -q --force-sync --optimized-fetch --no-tags --no-clone-bundle -j$(nproc --all) "$@"; }
 
 # List lib dependencies of any lib/bin
 list_blob_deps() { readelf -d $1 | grep "\(NEEDED\)" | sed -r "s/.*\[(.*)\]/\1/"; }
-
-# Prevent others from writing shit on to my terminal
-mesg n
 
 export TZ='Asia/Kolkata'
 export USE_CCACHE=1
@@ -85,26 +71,27 @@ EOF
 
 # Add android sdk to path
 cat <<'EOF' >> .profile
- 
+
 # Add Android SDK platform tools to path
 if [ -d "$HOME/platform-tools" ] ; then
     PATH="$HOME/platform-tools:$PATH"
 fi
 EOF
 
-# Set env from .bashrc and .profile
-source .profile
-source $sh_rc
-echo "Done"
+# Unlimited history file
+sed -i 's/HISTSIZE=.*/HISTSIZE=-1/g' $sh_rc
+sed -i 's/HISTFILESIZE=.*/HISTFILESIZE=-1/g' $sh_rc
+
+echo -e "Done."
 
 # Increase tmux scrollback buffer size
-echo "set-option -g history-limit 6000" >> ~/.tmux.conf
+echo "set-option -g history-limit 6000" >> .tmux.conf
 
 ###
 ### IMPORTANT !!! REPLACE WITH YOUR PERSONAL DETAILS IF NECESSARY
 ###
 # Configure git
-echo -e "\n================== CONFIGURING GIT ==================\n"
+echo -e "\nSetting up Git..."
 
 if [[ $USER == "adithya" ]]; then
 git config --global user.email "gh0strider.2k18.reborn@gmail.com"
@@ -126,13 +113,11 @@ git config --global alias.rb 'rebase'
 git config --global alias.rs 'reset'
 git config --global alias.ck 'checkout'
 git config --global credential.helper 'cache --timeout=99999999'
-echo "Done"
-
-# Prevent others from writing shit on to my terminal
-mesg n
+echo "Done."
 
 # Done!
-echo -e "\nALL DONE. Now sync sauces & start baking! \n"
+echo -e "\nALL DONE. Now sync sauces & start baking!"
+echo -e "Please relogin or run \`source ~/$sh_rc && source ~/.profile\` for environment changes to take effect."
 
 # Go back to original dir
-cd "$orig_dir" || return
+cd "$orig_dir"
