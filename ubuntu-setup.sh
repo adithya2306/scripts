3d73f699fd20122b4e033a881352de2b9318bdea
Adithya <gh0strider.2k18.reborn@gmail.com>
2022-10-03 15:58:40 +0530

    Set --retry-fetches=5 for repo sync
    Sometimes due to network or server issues, this is required (especially when syncing AOSP).

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -70,7 +70,7 @@ fi
 cat <<'EOF' >> $sh_rc
 
 # Super-fast repo sync
-repofastsync() { time schedtool -B -e ionice -n 0 `which repo` sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle -j$(nproc --all) "$@"; }
+repofastsync() { time schedtool -B -e ionice -n 0 `which repo` sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --retry-fetches=5 -j$(nproc --all) "$@"; }
 
 # List lib dependencies of any lib/bin
 list_blob_deps() { readelf -d $1 | grep "\(NEEDED\)" | sed -r "s/.*\[(.*)\]/\1/"; }
